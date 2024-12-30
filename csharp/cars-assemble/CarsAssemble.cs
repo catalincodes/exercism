3a775093b2bf0df6addc1d1f80a250cfa9383e0d
Catalin Codes <12548925+catalincodes@users.noreply.github.com>
2024-12-29 21:07:54 -0500

    Completed CarsAssemble exercism

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -4,16 +4,31 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
-    }
-    
-    public static double ProductionRatePerHour(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        if (speed == 0)
+        {
+            return 0.0;
+        }
+        else if (speed >= 1 && speed <= 4)
+        {
+            return 1.0;
+        }
+        else if (speed >=5 && speed <= 8)
+        {
+            return 0.9;
+        }
+        else if (speed == 9)
+        {
+            return 0.8;
+        }
+        else if (speed == 10)
+        {
+            return 0.77;
+        }
     }
 
-    public static int WorkingItemsPerMinute(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
-    }
+    public static double ProductionRatePerHour(int speed) => 
+        221 * speed * SuccessRate(speed);
+
+    public static int WorkingItemsPerMinute(int speed) => 
+        (int)(ProductionRatePerHour(speed) / 60.0);
 }
